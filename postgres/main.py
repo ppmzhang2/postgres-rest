27d4257b689035f77255509dc86b3eb0075a7198
ZHANG, Meng <ztz2000@gmail.com>
2021-02-24 17:31:05 +0800

    [new] init DB method

--- postgres/main.py
+++ postgres/main.py
@@ -1,12 +1,27 @@
 from datetime import datetime
 
-from fastapi import FastAPI, HTTPException
+from fastapi import HTTPException
 
 from postgres import cfg
 from postgres.models.dao import Dao
 
-app = FastAPI()
+from .middleware import app
+
 _dao = Dao()
+
+
+@app.post(f'{cfg.REST_URL_PREFIX}/init/')
+async def init_db():
+    try:
+        _dao.drop_all()
+        _dao.create_all()
+    except Exception as err:
+        raise HTTPException(
+            status_code=500,
+            detail='cannot execute now',
+        ) from err
+    else:
+        return {'message': 'success'}
 
 
 @app.get(f'{cfg.REST_URL_PREFIX}/sales')
