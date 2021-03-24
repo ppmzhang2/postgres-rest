99bd1d584a88eb86e2bc2825c2330d7bb9a00eea
ZHANG, Meng <ztz2000@gmail.com>
2021-03-24 16:58:55 +0800

    [new] API Exception handling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import logging
+
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
 from app import cfg
 from app.middleware import app
 from app.routers import category, config, summary
+from app.schema.api_exception import ApiException
+
+logging.config.dictConfig(cfg.LOGGING)
+logger = logging.getLogger()
 
 app.include_router(
     config.router,
@@ -25,3 +34,14 @@ app.include_router(
         'detail': 'not found'
     }},
 )
+
+
+@app.exception_handler(ApiException)
+async def api_exception_handler(_: Request, exception: ApiException):
+    content = {
+        'category': exception.category,
+        'message': exception.message,
+    }
+    response = JSONResponse(status_code=exception.status_code, content=content)
+    logger.error({'error': repr(exception)})
+    return response
